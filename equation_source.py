3b7eb5453e0836955189aef8d02d1a2de06f1c28
Charles Yong <charlesy@mit.edu>
2024-12-12 18:54:19 -0500

    Add equation source templates.

---
+++ equation_source.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+
+BBL = Book("Ball Bearing Lubrication: The Elastohydrodynamics of Elliptical Contacts")
+BDM = Book("Bearing Design in Machinery: Engineering Tribology and Lubrication")
+ECBT = Book("Essential Concepts of Bearing Technology")
+ACBT = Book("Advanced Concepts of Bearing Technology")
+
+
+class EquationSource:
+    def __init__(self, book, page):
+        self.book = book
+        self.page = page
